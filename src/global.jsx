5ed0b67788a5fa2c034545181dea1eb01e896fcf
Ruslan Nikolaev <biscvi@gmail.com>
2025-08-16 15:43:04 +0300

    Меню навигации, списки в JSX, метод массивов map

--- src/global.jsx
+++ src/global.jsx
@@ -8,6 +8,7 @@ export default function (props) {
   const {
     children,
     title,
+    url,
   } = props
 
   return (
@@ -40,7 +41,7 @@ export default function (props) {
           href="/site.webmanifest"
         />
       </Head>
-      <Header />
+      <Header url={url} />
       <Content>
         {children}
       </Content>
